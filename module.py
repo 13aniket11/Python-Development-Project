9971418f5f6b6d99c28608fb706e45983fce8566
13aniket11 <59722228+13aniket11@users.noreply.github.com>
2020-11-06 17:31:05 +0530

    Project Discription

---
+++ module.py
@@ -0,0 +1,12 @@
+# 6 November 2020
+# Python Developement
+#
+# modules Discussed
+# Pygame
+# Panda 3D
+# KIVY
+# Platform Android or Cross
+# Dimension 3D
+# Total 4 Team Members
+# last date to submit Project is January end
+# 
